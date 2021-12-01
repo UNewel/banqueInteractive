efc2adcd734e4c407c432b8e72065fa07c652f30
UNewel <75020809+UNewel@users.noreply.github.com>
2021-12-01 10:58:30 +0100

    Initial commit

---
+++ src/compte.java
@@ -0,0 +1,21 @@
+public class compte {
+
+    private int numero;
+    private float solde;
+
+    public void depot(float valeur) {
+
+    }
+
+    public void retrait(float valeur) {
+
+    }
+
+    public float getSolde() {
+        return this.solde;
+    }
+
+    public void afficherSolde() {
+
+    }
+}
